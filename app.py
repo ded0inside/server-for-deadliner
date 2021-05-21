3d67b4cd3af4f643eda3890cdf4a8d5d3ae2be2e
Дулькин Илья <ilia.dulkin@gmail.com>
2021-05-21 17:53:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import time
-from Schedule import Schedule
-from Deadline import Deadline
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///server.db'
